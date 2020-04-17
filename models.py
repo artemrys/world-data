856bea61c2835a7d54beeea298752e141263b8c0
Artem Rys <rysartem@gmail.com>
2020-04-18 00:06:19 +0200

    Add coordinates to city model.

--- models.py
+++ models.py
@@ -1,13 +1,25 @@
+from google.cloud import firestore
+
+
 class City(object):
-    def __init__(self, name, country_ref):
+    def __init__(self,
+                 name: str,
+                 coordinates: firestore.GeoPoint,
+                 country_ref: firestore.DocumentReference):
         self.name = name
+        self.coordinates = coordinates
         self.country_ref = country_ref
 
     @classmethod
     def from_dict(cls, source):
-        return City(source["name"], source["country"])
+        return City(
+            source["name"],
+            source["coordinates"],
+            source["country"])
 
     def to_dict(self):
         return {
             "name": self.name,
+            "latitude": self.coordinates.latitude,
+            "longitude": self.coordinates.longitude,
         }
